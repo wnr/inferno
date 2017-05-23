da73d1bcf1b4904d3ca9044d0508cd1f4d4819be
Luke Sheard <me@lukesheard.com>
2017-05-23 08:18:24 +0100

    Fix alias and lerna

--- scripts/rollup/plugins/alias.js
+++ scripts/rollup/plugins/alias.js
@@ -1,16 +1,18 @@
-const { resolve } = require('path');
+const { resolve, join } = require('path');
 const alias = require('rollup-plugin-alias');
 
+const ROOT = join(ROOT, '');
+
 module.exports = alias({
-	inferno: resolve(__dirname, '../packages/inferno/dist/index.js'),
-	'inferno-compat': resolve(__dirname, '../packages/inferno-compat/dist/index.js'),
-	'inferno-component': resolve(__dirname, '../packages/inferno-component/dist/index.js'),
-	'inferno-create-class': resolve(__dirname, '../packages/inferno-create-class/dist/index.js'),
-	'inferno-create-element': resolve(__dirname, '../packages/inferno-create-element/dist/index.js'),
-	'inferno-hyperscript': resolve(__dirname, '../packages/inferno-hyperscript/dist/index.js'),
-	'inferno-mobx': resolve(__dirname, '../packages/inferno-mobx/dist/index.js'),
-	'inferno-redux': resolve(__dirname, '../packages/inferno-redux/dist/index.js'),
-	'inferno-router': resolve(__dirname, '../packages/inferno-router/dist/index.js'),
-	'inferno-server': resolve(__dirname, '../packages/inferno-server/dist/index.js'),
-	'inferno-shared': resolve(__dirname, '../packages/inferno-shared/dist/index.js'),
+	inferno: resolve(ROOT, 'packages/inferno/dist/index.js'),
+	'inferno-compat': resolve(ROOT, 'packages/inferno-compat/dist/index.js'),
+	'inferno-component': resolve(ROOT, 'packages/inferno-component/dist/index.js'),
+	'inferno-create-class': resolve(ROOT, 'packages/inferno-create-class/dist/index.js'),
+	'inferno-create-element': resolve(ROOT, 'packages/inferno-create-element/dist/index.js'),
+	'inferno-hyperscript': resolve(ROOT, 'packages/inferno-hyperscript/dist/index.js'),
+	'inferno-mobx': resolve(ROOT, 'packages/inferno-mobx/dist/index.js'),
+	'inferno-redux': resolve(ROOT, 'packages/inferno-redux/dist/index.js'),
+	'inferno-router': resolve(ROOT, 'packages/inferno-router/dist/index.js'),
+	'inferno-server': resolve(ROOT, 'packages/inferno-server/dist/index.js'),
+	'inferno-shared': resolve(ROOT, 'packages/inferno-shared/dist/index.js'),
 });
